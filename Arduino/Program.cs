3dad27458336509e8cfa77e3109d6b0c9f7639b7
Stefan <psa9710@gmail.com>
2018-05-02 17:36:20 +0300

    Added arduino Branch

---
+++ Arduino/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Threading;
+
+namespace Arduino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Connecting");
+            SerialPort serial = new SerialPort("COM3",9600);
+            serial.Open();
+            Console.WriteLine("Connection Succesfull!");
+            Console.WriteLine("Set LED ON HIGH");
+            serial.Write("H");
+            Thread.Sleep(4000);
+            Console.WriteLine("SET LED ON LOW");
+            serial.Write("L");
+            Thread.Sleep(2000);
+            serial.Close();
+        }
+    }
+}
--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,33 +1,73 @@
-#include "SoftwareSerial.h";
-int bluetoothTx = 2;
-int bluetoothRx = 3;
+#include <SoftwareSerial.h>
 
+int bluetoothTx = 2;  // TX-O pin of bluetooth mate, Arduino D2
+int bluetoothRx = 3;  // RX-I pin of bluetooth mate, Arduino D3
+
+int ledGreen = 10;
+int ledYellow = 9;
+int ledRed = 8;
 SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
 
 void setup()
 {
+  Serial.begin(9600);  // Begin the serial monitor at 9600bps
 
-  Serial.begin(9600);
-
-
-  bluetooth.begin(115200);
-  bluetooth.print("$$$");
-  delay(100);
-  bluetooth.println("U,9600,N");
-  bluetooth.begin(9600);
+  bluetooth.begin(115200);  // The Bluetooth Mate defaults to 115200bps
+  bluetooth.print("$");  // Print three times individually
+  bluetooth.print("$");
+  bluetooth.print("$");  // Enter command mode
+  delay(100);  // Short delay, wait for the Mate to send back CMD
+  bluetooth.println("U,9600,N");  // Temporarily Change the baudrate to 9600, no parity
+  // 115200 can be too fast at times for NewSoftSerial to relay the data reliably
+  bluetooth.begin(9600);  // Start bluetooth serial at 9600
+  pinMode(ledGreen, OUTPUT);
+  pinMode(ledYellow, OUTPUT);
+  pinMode(ledRed, OUTPUT);
 }
 
 void loop()
 {
-  if(bluetooth.available()) {
-    char toSend = (char)bluetooth.read();
-    Serial.print(toSend);
-  }
+  if (bluetooth.available()) // If the bluetooth sent any characters
+  {
+    char c = (char)bluetooth.read();
+    // Send any characters the bluetooth prints to the serial monitor
+    Serial.print(c);
+    SwitchLedGreen(c);
+    SwitchLedYellow(c);
+    SwitchLedRed(c);
 
-  if(Serial.available()) {
-    char toSend = (char)Serial.read();
-    bluetooth.print(toSend);
   }
-  Serial.print("test");
+  if (Serial.available()) // If stuff was typed in the serial monitor
+  {
+    // Send any characters the Serial monitor prints to the bluetooth
+    bluetooth.print((char)Serial.read());
+  }
+  // and loop forever and ever!
 }
- 
+
+void SwitchLedGreen(char c)
+{
+  if (c == 'H')
+    digitalWrite(ledGreen, HIGH);
+  else
+    digitalWrite(ledGreen, LOW);
+}
+
+void SwitchLedYellow(char c)
+{
+  if (c == 'H')
+    digitalWrite(ledYellow, HIGH);
+  else
+    digitalWrite(ledYellow, LOW);
+}
+
+
+void SwitchLedRed(char c)
+{
+  if (c == 'H')
+    digitalWrite(ledRed, HIGH);
+  else
+    digitalWrite(ledRed, LOW);
+}
+
+
